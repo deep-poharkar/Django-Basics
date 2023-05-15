c8ea6b46bf40872a97c5a39d0f31f058c3263c06
deep-poharkar <deeppoharkar21@gmail.com>
2023-05-15 18:38:42 +0530

    added some frontend

---
+++ .history/home/views_20230514003753.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    context = {
+        'variable1':'this is sent',
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+ 
